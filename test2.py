b87dbc4874f0c093cb6f6f96d915efac715f25bc
AnnaBulavina <bulavina65@gmail.com>
2023-06-23 18:00:10 +0300

    add our test

--- test2.py
+++ test2.py
@@ -1,5 +1,6 @@
 import driver as driver
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,7 +9,12 @@ URL = 'https://www.saucedemo.com/'
 LOGIN='standard_user'
 PASSWORD='secret_sauce'
 def get_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1920,800")
+    chrome_options.add_argument('--headless')
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.maximize_window()
+    driver.implicitly_wait(10)
     return driver
 def open_page(driver, url):
     driver.get(url)
