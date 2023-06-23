991203c8ccff999d446471f1ce05eb9d49bb8a18
AnnaBulavina <bulavina65@gmail.com>
2023-06-23 16:37:46 +0300

    add our test

---
+++ test2.py
@@ -0,0 +1,37 @@
+import driver as driver
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+URL = 'https://www.saucedemo.com/'
+LOGIN='standard_user'
+PASSWORD='secret_sauce'
+def get_driver():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    return driver
+def open_page(driver, url):
+    driver.get(url)
+def get_element_by_id(driver, locator):
+    return driver.find_element(By.ID, locator)
+def element_click(driver, locator):
+    element = get_element_by_id(driver, locator)
+    element.click()
+def element_send_keys(driver, locator, text):
+    element = get_element_by_id(driver, locator)
+    element.send_keys(text)
+
+def open_login_page(driver):
+    element_click(driver, 'user-name')
+
+def login(driver, login, password):
+element_send_keys(driver, 'user-name', LOGIN)
+element_send_keys(driver, 'password', PASSWORD)
+element_click(driver, 'login-button')
+
+driver = get_driver()
+open_page(driver, URL)
+open_login_page(driver)
+login(driver=driver, login=LOGIN, password=PASSWORD)
+
+driver.quit()
---
+++ test_our_site.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+URL = 'https://www.saucedemo.com/'
+LOGIN='standard_user'
+PASSWORD='secret_sauce'
+def get_driver():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    return driver
+def open_page(driver, url):
+    driver.get(url)
+def get_element_by_id(driver, locator):
+    return driver.find_element(By.ID, locator)
+def element_click(driver, locator):
+    element = get_element_by_id(driver, locator)
+    element.click()
+def element_send_keys(driver, locator, text):
+    element = get_element_by_id(driver, locator)
+    element.send_keys(text)
+#1.
+driver = get_driver()
+open_page(driver, URL)
+#2,3
+element_send_keys(driver, 'user-name', LOGIN)
+if element_send_keys(driver, 'password', PASSWORD):
+#4
+element_click(driver, 'login-button')
